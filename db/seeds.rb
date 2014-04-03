3e87c61d266468c73d68627e21d6454af875dcfe
Tim Pryor <tdpryor@gmail.com>
2014-04-03 12:16:01 -0400

    Add show page for videos

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,28 @@
 
 Video.create(:title => 'Monk',
              :description => 'Adrian Monk is a brilliant San Francisco detective...',
-             :small_cover_url => 'tmp/monk.jpg',
-           :large_cover_url => 'tmp/monk_large')
+             :small_cover_url => '/tmp/monk.jpg',
+             :large_cover_url => '/tmp/monk_large.jpg')
 
+Video.create(:title => 'Family Guy',
+             :description => 'In a wacky Rhode Island town, a dysfunctional family...',
+             :small_cover_url => '/tmp/family_guy.jpg')
+
+
+Video.create(:title => 'Futurama',
+             :description => 'Fry, a pizza guy, is accidentally frozen in 1999...',
+             :small_cover_url => '/tmp/futurama.jpg')
+
+Video.create(:title => 'South Park',
+             :description => 'Follows the misadventures of four irreverent grade schoolers...',
+             :small_cover_url => '/tmp/south_park.jpg')
+
+
+Video.create(:title => 'Monk',
+             :description => 'Adrian Monk is a brilliant San Francisco detective...',
+             :small_cover_url => '/tmp/monk.jpg',
+             :large_cover_url => '/tmp/monk_large')
+
+Video.create(:title => 'Family Guy',
+             :description => 'In a wacky Rhode Island town, a dysfunctional family...',
+             :small_cover_url => '/tmp/family_guy.jpg')
